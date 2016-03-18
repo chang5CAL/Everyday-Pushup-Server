e6dd30a3e4be3ca327d9fed0f825dd785ef9c75e
chang5CAL <ginka13@gmail.com>
2016-03-17 18:16:27 -0700

    initial sending code

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,17 @@ router.get('/', function(req, res, next) {
 router.post('/register', function(req, res) {
 	if (req.body.platform == 'ios' || req.body.platform == 'android') {
 		console.log(req.body)
+		res.json({message: "in theory, you're registered!"})
+
+		/*var messgae = new gcm.Message();
+		message.add("fitness-extra", "nothing!");
+		var registerToken = [req.body.token];
+
+		var sender = new gcm.Sender("1111111") // api key
+		sender.send(message, { registrationTokens: registerToken,}, function(err, res) {
+			if (err) console.log(err);
+			else console.log(response)
+		});*/
 	} else {
 		res.status(400);
 		res.json({
