f2bd9e4758140c5495c4a6b214500a4157091ad5
chang5CAL <ginka13@gmail.com>
2016-03-17 17:34:15 -0700

    basic header files

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var gcm = require("node-gcm");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function(req, res) {
+	if (req.body.platform == 'ios' || req.body.platform == 'android') {
+		console.log(req.body)
+	} else {
+		res.status(400);
+		res.json({
+			message:"invalid paramaters"
+		})
+	}
 });
 
 module.exports = router;
