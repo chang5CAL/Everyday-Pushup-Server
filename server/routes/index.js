d076b48cae58ef9f36cf3fad8fecf8a04f551cb4
chang5CAL <ginka13@gmail.com>
2016-03-21 20:05:15 -0700

    added update checks

--- server/routes/index.js
+++ server/routes/index.js
@@ -49,8 +49,11 @@ router.post('/update-schedule', function(req, res) {
 	// update user with the 
 	db.exercises.update(
 		{ "token": req.body.token},
-		{$set:{"dates": JSON.stringify(req.body.dates)}}
-	)
+		{$set:{"dates": req.body.dates}}
+	, function(err) {
+		console.log(err);
+		res.send("yay");
+	});
 });
 
 
