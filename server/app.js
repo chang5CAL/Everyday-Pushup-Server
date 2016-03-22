addf06beab1857c46a1060e49e77a86c55a4944f
chang5CAL <ginka13@gmail.com>
2016-03-21 20:31:50 -0700

    possible fix

--- server/app.js
+++ server/app.js
@@ -97,7 +97,7 @@ function addNewQuery() {
     console.log(docs);
     for(var i = 0; i < docs.length; i++){
       console.log("going through list");
-      var list = JSON.parse(docs[i].dates[day].list);
+      var list = docs[i].dates[day].list;
       for(var r = 0; r < docs[i].dates[day].workouts; r++) {
         console.log("starting to look at list");
         if (list.length == 0) {
